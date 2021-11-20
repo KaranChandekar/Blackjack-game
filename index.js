287b91429fc5525b4d966594449c87608d864885
Karan Chandekar <93200960+KaranChandekar@users.noreply.github.com>
2021-11-20 17:41:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function startGame() {
 function renderGame() {
     cardsEl.textContent = "Cards: ";
     for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " ";
+        cardsEl.textContent += cards[i] + " " + " ";
     }
     sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
